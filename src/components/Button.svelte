22805a8eea6c2a88d722c559efedcbcc43c5c478
Larus Thor <lalli.oni@gmail.com>
2022-12-30 11:30:50 +0100

    Add Button component

---
+++ src/components/Button.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    export let label: string;
+    export let disabled: boolean = false;
+</script>
+
+
+<label
+    class="text-white h-full border border-white rounded-md m-1 p-1"
+    class:underline="{disabled === false}"
+    class:text-gray="{disabled}"
+    class:underline-offset-2="{disabled === false}"
+    class:hover:cursor-pointer="{disabled === false}"
+    {disabled}
+>
+    {label}
+    <input on:click {disabled} type="button">
+</label>
