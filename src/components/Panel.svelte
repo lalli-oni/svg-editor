4c0c3a15daaf7103fa6ebef887974f33e84d9048
Larus Thor <lalli.oni@gmail.com>
2022-12-30 11:31:09 +0100

    Add Panel component

---
+++ src/components/Panel.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let title: string;
+</script>
+
+<div class="border border-white rounded-md p-2 flex flex-col gap-2 text-white">
+    {#if title}
+        <h2>{title}</h2>
+    {/if}
+    <slot></slot>
+</div>
